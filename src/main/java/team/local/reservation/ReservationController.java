e1d4466e00973208836eac05afa4aba2c1d8fa64
k.melnikov <k.melnikov@foodtech.team>
2026-05-02 16:09:05 +0300

    init commit

---
+++ src/main/java/team/local/reservation/ReservationController.java
@@ -0,0 +1,7 @@
+package team.local.reservation;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReservationController {
+}
---
+++ src/main/java/team/local/reservation/ReservationService.java
@@ -0,0 +1,7 @@
+package team.local.reservation;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReservationService {
+}
