240ec82b04fd5fe153640800ee9ffcf291070664
k.melnikov <k.melnikov@foodtech.team>
2026-05-02 17:09:49 +0300

    feat: add 2 endpoint to controller

--- src/main/java/team/local/reservation/ReservationController.java
+++ src/main/java/team/local/reservation/ReservationController.java
@@ -1,7 +1,39 @@
 package team.local.reservation;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
+
+@Slf4j
 @RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/")
 public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        log.info("Use [GET] /api/v1/health");
+        return ResponseEntity.ok("{\"status\": \"ok\"}");
+    }
+
+    @GetMapping("/reservation/{id}")
+    public ResponseEntity<HashMap<String, Object>> getReservationByID(@PathVariable Integer id) {
+
+        log.info("USE [GET] /reservation/{}", id.toString());
+
+        HashMap<String, Object> response = new HashMap<>();
+        String reservation = reservationService.getReservationById(id);
+        response.put("reservationId", reservation);
+
+        return ResponseEntity.ok(response);
+    }
 }
--- src/main/java/team/local/reservation/ReservationService.java
+++ src/main/java/team/local/reservation/ReservationService.java
@@ -1,7 +1,13 @@
 package team.local.reservation;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class ReservationService {
+    public String getReservationById(Integer id) {
+        log.info("Getting reservation...");
+        return "reservation-%s".formatted(id);
+    }
 }
