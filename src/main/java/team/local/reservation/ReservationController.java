e8cc98f90460e8805d78a99ceeb55975e1e34345
k.melnikov <k.melnikov@foodtech.team>
2026-05-02 20:06:18 +0300

    feat: removed logger imports

--- src/main/java/team/local/reservation/ReservationController.java
+++ src/main/java/team/local/reservation/ReservationController.java
@@ -2,8 +2,6 @@ package team.local.reservation;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
